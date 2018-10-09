36a246ed872e1aaf5aeac620849b8c1ff50efbf4
omeir1 <ohad.meir@intel.com>
2018-10-09 16:13:05 +0300

    prepare_submission.py for cells

--- official/resnet/inference/prepare_submission.py
+++ official/resnet/inference/prepare_submission.py
@@ -8,24 +8,24 @@ etc
 import os
 
 import cv2
+from tqdm import tqdm
 
 from official.resnet.inference.common import get_parser, get_classes_desc
 from official.resnet.inference.model_inference import ModelInference
 
 
 def main(args):
-    classes_desc, num_classes = get_classes_desc(args.classes_path, map_to_id=True)
+    num_classes = 28
 
     all_classifications = []
 
     with ModelInference(num_classes=num_classes, resnet_size=args.resnet_size, model_path=args.model_path) as model:
-        for file in os.listdir(args.images_dir):
+        for file in tqdm(os.listdir(args.images_dir)):
             image_path = os.path.join(args.images_dir, file)
             image = cv2.imread(image_path)
 
             found = model.infer(image=image)
 
-            found = [classes_desc[index] for index in found]
             all_classifications += [(file, found)]
 
     model_path = args.model_path.split('\\')
@@ -33,8 +33,8 @@ def main(args):
                                '{}{}.txt'.format(model_path[-2], model_path[-1].replace('model.ckpt', '')))
     with open(output_path, 'w') as f:
         f.write('image_id,labels')
-        for file, found in all_classifications:
-            f.write('{},{}\n'.format(file.replace('.jpg', ''), ' '.join(found)))
+        for file, found in tqdm(all_classifications):
+            f.write('{},{}\n'.format(file.replace('_rgba.png', ''), ' '.join(found)))
 
 
 if __name__ == '__main__':
