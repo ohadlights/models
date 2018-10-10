cc1d272e7becdbcba71e0886ff50e29c8977545d
omeir1 <ohad.meir@intel.com>
2018-10-10 13:24:41 +0300

    merge_submissions.py

--- official/resnet/inference/merge_submissions.py
+++ official/resnet/inference/merge_submissions.py
@@ -21,6 +21,6 @@ if __name__ == '__main__':
     main(output_path=r'X:\OpenImages\InclusiveChallenge\submissions\merged\submission2.txt',
          submissions_dir=r'X:\OpenImages\InclusiveChallenge\submissions',
          merge_files=['range_more_100k_val_multi-2866309',
-                      'range_in_10k_100K_multi-1060309',
-                      'range_in_1k_10K_multi-1112264',
+                      'range_in_10k_100K_multi-2743824',
+                      'range_in_1k_10K_multi-2760289',
                       'range_in_100_1k_multi_label-908655'])
