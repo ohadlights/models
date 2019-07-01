d3a70b9b1834e1ac4cee902a48a6ed371ad875fc
omeir1 <ohad.meir@intel.com>
2019-07-01 14:59:30 +0300

    resnet_v1_26

--- research/slim/nets/resnet_v1.py
+++ research/slim/nets/resnet_v1.py
@@ -279,7 +279,7 @@ def resnet_v1_block(scope, base_depth, num_units, stride):
   }])
 
 
-def resnet_v1_17(inputs,
+def resnet_v1_26(inputs,
                  num_classes=None,
                  is_training=True,
                  global_pool=True,
@@ -289,8 +289,8 @@ def resnet_v1_17(inputs,
                  min_base_depth=8,
                  depth_multiplier=1,
                  reuse=None,
-                 scope='resnet_v1_17'):
-  """ResNet-17 model of [1]. See resnet_v1() for arg and return description."""
+                 scope='resnet_v1_26'):
+  """ResNet-26 model of [1]. See resnet_v1() for arg and return description."""
   depth_func = lambda d: max(int(d * depth_multiplier), min_base_depth)
   blocks = [
       resnet_v1_block('block1', base_depth=depth_func(64), num_units=2,
@@ -307,69 +307,7 @@ def resnet_v1_17(inputs,
                    include_root_block=True, spatial_squeeze=spatial_squeeze,
                    store_non_strided_activations=store_non_strided_activations,
                    reuse=reuse, scope=scope)
-resnet_v1_17.default_image_size = resnet_v1.default_image_size
-
-
-def resnet_v1_20(inputs,
-                 num_classes=None,
-                 is_training=True,
-                 global_pool=True,
-                 output_stride=None,
-                 spatial_squeeze=True,
-                 store_non_strided_activations=False,
-                 min_base_depth=8,
-                 depth_multiplier=1,
-                 reuse=None,
-                 scope='resnet_v1_20'):
-  """ResNet-20 model of [1]. See resnet_v1() for arg and return description."""
-  depth_func = lambda d: max(int(d * depth_multiplier), min_base_depth)
-  blocks = [
-      resnet_v1_block('block1', base_depth=depth_func(64), num_units=2,
-                      stride=2),
-      resnet_v1_block('block2', base_depth=depth_func(128), num_units=2,
-                      stride=2),
-      resnet_v1_block('block3', base_depth=depth_func(256), num_units=3,
-                      stride=2),
-      resnet_v1_block('block4', base_depth=depth_func(512), num_units=2,
-                      stride=1),
-  ]
-  return resnet_v1(inputs, blocks, num_classes, is_training,
-                   global_pool=global_pool, output_stride=output_stride,
-                   include_root_block=True, spatial_squeeze=spatial_squeeze,
-                   store_non_strided_activations=store_non_strided_activations,
-                   reuse=reuse, scope=scope)
-resnet_v1_20.default_image_size = resnet_v1.default_image_size
-
-
-def resnet_v1_38(inputs,
-                 num_classes=None,
-                 is_training=True,
-                 global_pool=True,
-                 output_stride=None,
-                 spatial_squeeze=True,
-                 store_non_strided_activations=False,
-                 min_base_depth=8,
-                 depth_multiplier=1,
-                 reuse=None,
-                 scope='resnet_v1_38'):
-  """ResNet-38 model of [1]. See resnet_v1() for arg and return description."""
-  depth_func = lambda d: max(int(d * depth_multiplier), min_base_depth)
-  blocks = [
-      resnet_v1_block('block1', base_depth=depth_func(64), num_units=2,
-                      stride=2),
-      resnet_v1_block('block2', base_depth=depth_func(128), num_units=3,
-                      stride=2),
-      resnet_v1_block('block3', base_depth=depth_func(256), num_units=5,
-                      stride=2),
-      resnet_v1_block('block4', base_depth=depth_func(512), num_units=2,
-                      stride=1),
-  ]
-  return resnet_v1(inputs, blocks, num_classes, is_training,
-                   global_pool=global_pool, output_stride=output_stride,
-                   include_root_block=True, spatial_squeeze=spatial_squeeze,
-                   store_non_strided_activations=store_non_strided_activations,
-                   reuse=reuse, scope=scope)
-resnet_v1_38.default_image_size = resnet_v1.default_image_size
+resnet_v1_26.default_image_size = resnet_v1.default_image_size
 
 
 def resnet_v1_50(inputs,
