a5e2d07c252b769c0c9e9de5ddb40734e1046bfe
omeir1 <ohad.meir@intel.com>
2019-12-02 13:14:11 +0200

    create_widerface_fddb_tf_tasqai_noface_record.py

--- research/object_detection/dataset_tools/create_widerface_fddb_tf_tasqai_noface_record.py
+++ research/object_detection/dataset_tools/create_widerface_fddb_tf_tasqai_noface_record.py
@@ -1,4 +1,4 @@
-import random
+import cv2
 from tqdm import tqdm
 
 import contextlib2
@@ -6,35 +6,22 @@ from object_detection.dataset_tools import tf_record_creation_util
 from object_detection.dataset_tools.create_wider_face_tf_record import\
     process_list as process_widerface, create_tf_example
 from object_detection.dataset_tools.create_widerface_fddb_tf_record import process_list_fddb
+from object_detection.dataset_tools.create_widerface_fddb_tf_tasqai_record import process_list_tasqai
 
 
-def process_list_tasqai(path, min_size):
-    annotations = [l.strip().split(',') for l in open(path)]
-    random.seed(0)
-    annotations = random.sample(annotations, k=5000)
+def process_list_noface(path):
+    paths = [l.strip() for l in open(path)]
     examples = []
-    num_filtered = 0
-    for a in tqdm(annotations, desc='Collecting TasqAI faces'):
-        path = a[0]
-        coords = [int(a) for a in a[1:]]
-        faces = []
-        for i in range(0, len(coords), 4):
-            face = coords[i:i+4]
-            face = [face[0], face[1], face[2] - face[0], face[3] - face[1]]
-            if (min_size == 0) or (face[2] > min_size and face[3] > min_size):
-                faces += [face]
-            else:
-                num_filtered += 1
-        if len(faces) > 0:
-            examples += [(path, faces)]
-    print('TasqAI filtered: {}'.format(num_filtered))
+    for p in tqdm(paths):
+        if cv2.imread(p) is not None:
+            examples += [(p, [])]
     return examples
 
 
 def main():
     num_shards = 5
     min_size = 20
-    output_path = r'X:\IJB-C\NIST_11\tf_object_detection_api\records\wider_face_fddb_tasqai\widerface_fddb_tasqai5K_train{}.record'\
+    output_path = r'X:\IJB-C\NIST_11\tf_object_detection_api\records\wider_face_fddb_tasqai_noface\widerface_fddb_tasqai_noface_train{}.record'\
         .format('' if min_size == 0 else '_filter{}'.format(min_size))
 
     examples = []
@@ -46,6 +33,7 @@ def main():
     examples += process_list_fddb(r'X:\fddb\FDDB-folds\FDDB-all_ellipseList.txt', min_size=min_size)
     examples += process_list_tasqai(r'X:\IJB-C\NIST_11\tf_object_detection_api\records\tasqai\tasqai_image_annotations_bbox_train.txt',
                                     min_size=min_size)
+    examples += process_list_noface(r'X:\IJB-C\NIST_11\tf_object_detection_api\records\nofaces\no_faces_list_train.txt')
 
     with contextlib2.ExitStack() as tf_record_close_stack:
         output_tfrecords = tf_record_creation_util.open_sharded_output_tfrecords(
